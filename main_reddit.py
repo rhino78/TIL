2f99e2eff65d7ead619631097e5a3d20a8fcf4ed
ryan shave <rshave@gmail.com>
2018-12-08 11:42:03 -0600

    retiring linuxbox :(

--- main_reddit.py
+++ main_reddit.py
@@ -1,26 +1,36 @@
+#!/usr/bin/env python
 import logging
 import tweet_with_db
 import praw
 import time
 import datetime
+
+
 redditlist = []
+
+
 def main():
-        logging.basicConfig(filename='tweet.log', level=logging.INFO)
-        st = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
-        logging.info('Tweet log for: '+ st)
-        r = praw.Reddit(user_agent='ryan_test_app')
-        subreddit = r.get_subreddit("todayilearned")
-        for submission in subreddit.get_hot(limit=20):
-                if len(submission.title) <=164:
-                        sub = submission.title
-                        redditlist.append(sub)
-        
-        redditlist.remove(redditlist[0])
-        for r in redditlist:
-                if tweet_with_db.tweet_it(r):
-                        logging.info('Finished')
-                        break
+    logging.basicConfig(filename='/home/ryan/github/TIL/tweet.log', level=logging.INFO)
+    st = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+    logging.info('Tweet log for: '+ st)
+    r = praw.Reddit(client_id='JgiAaoEUbf86tA',
+        client_secret='bz4rt2sUhhQs0suOR_2uq4QuIhA',
+        password='thomas08',
+        user_agent='tweet_bot',
+        username='rhino_78')
+
+    subreddit = r.subreddit("todayilearned")
+    for submission in subreddit.hot(limit=20):
+        if len(submission.title) <= 140:
+            sub = submission.title
+            redditlist.append(sub)
+
+    redditlist.remove(redditlist[0])
+    for r in redditlist:
+        if tweet_with_db.tweet_it(r):
+            logging.info('Finished')
+            break
 
 
 if __name__ == '__main__':
-        main()
+    main()
--- tweet_with_db.py
+++ tweet_with_db.py
@@ -3,39 +3,39 @@ import tweepy
 import sqlite3
 import tweet_creds
 def tweet_it(status):
-        auth = tweepy.OAuthHandler(tweet_creds.CONSUMER_KEY, tweet_creds.CONSUMER_SECRET)
-        auth.set_access_token(tweet_creds.ACCESS_KEY, tweet_creds.ACCESS_SECRET)
-        api = tweepy.API(auth)
-        
-        try:
-                 if not_in_db(status):
-                         api.update_status(status=status)
-                         log_it(status)
-                         return True
-                 else:
-                         return False
-                
-        except tweepy.TweepError as e:
-                logging.info(e)
-                logging.info(type(e))
-                logging.info(e.reason)
-                return False
-        finally:
-                logging.info('Tweeting: ' + status)
+    auth = tweepy.OAuthHandler(tweet_creds.CONSUMER_KEY, tweet_creds.CONSUMER_SECRET)
+    auth.set_access_token(tweet_creds.ACCESS_KEY, tweet_creds.ACCESS_SECRET)
+    api = tweepy.API(auth)
+    logging.basicConfig(filename='tweet.log', level=logging.INFO)
+    logging.info(status)
+    try:
+        if not_in_db(status):
+            api.update_status(status=status)
+            log_it(status)
+            return True
+        else:
+            return False
+    except tweepy.TweepError as e:
+        logging.info(e)
+        logging.info(type(e))
+        logging.info(e.reason)
+        return False
+    finally:
+        logging.info('Tweeting: ' + status)
 
 def not_in_db(status):
-        conn = sqlite3.connect(r"C:\Python34\test.db")
-        cursor = conn.cursor()
-        cursor.execute('select 1 from tweets where tweet = ?', (status,))
-        if cursor.fetchone():
-                return False
-        else:
-                return True
+    conn = sqlite3.connect(r"/home/ryan/github/TIL/til.db")
+    cursor = conn.cursor()
+    cursor.execute('select 1 from tweets where tweet = ?', (status,))
+    if cursor.fetchone():
+        return False
+    else:
+        return True
 
 def log_it(status):
-        conn = sqlite3.connect(r"C:\Python34\test.db")
-        cursor = conn.cursor()
-        cursor.execute('insert into tweets(tweet, dateoftweet) values(?, CURRENT_TIMESTAMP)', (status,))
-        conn.commit()
-        conn.close()
+    conn = sqlite3.connect(r"/home/ryan/github/TIL/til.db")
+    cursor = conn.cursor()
+    cursor.execute('insert into tweets(tweet, dateoftweet) values(?, CURRENT_TIMESTAMP)', (status,))
+    conn.commit()
+    conn.close()
 
